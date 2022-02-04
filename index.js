9bfa994604ff0dc3200911590a6056cbab18db85
Dagmawi Babi <44819176+dagmawibabi@users.noreply.github.com>
2022-02-04 03:23:40 +0300

    added covid stats and crypto prices

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
 
-//const token = '5208477658:AAHL6kfn6C7v_ngn41xjIAoL9MCbBNsjhO8';
+const token = '5208477658:AAHL6kfn6C7v_ngn41xjIAoL9MCbBNsjhO8';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -11,24 +13,115 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 });
 
 // Start
+let commands = {
+    "reply_markup": {
+        "keyboard": [
+            ["Hello", "Good Morning"],
+            ["Send Photo", "Send Song"], 
+            ["Covid Stats", "Crypto Prices"],
+            ["Good Night", "Bye"],
+        ]
+    }
+};
+
+// Start
 bot.on(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     let greetings = "Hello 👋 \n" +  msg.chat.first_name.toString();
-    bot.sendMessage(chatId, greetings);
+    bot.sendMessage(chatId, greetings, commands);
 });
 
 // Covid API
-let covidData = "EMPTY COVID DATA!";
-async function covidAPI()  {
-    request('https://corona.lmao.ninja/v2/countries/Ethiopia', function (error, response, body) {
+async function covidAPI(chatId)  {
+    await request('https://corona.lmao.ninja/v2/countries/Ethiopia', function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log(body);
-            covidData = body;
+            let bodyJSON = JSON.parse(body);
+            let covidData = "COVID CASES \n\n" + bodyJSON["country"] + "\n\nToday: " + bodyJSON["todayCases"] + "\nTotal: " + bodyJSON["cases"];
+covidData = `
+COVID CASES
+
+${bodyJSON["country"]}
+
+Today
+Deaths: ${bodyJSON["todayDeaths"]}
+Cases: ${bodyJSON["todayCases"]}
+Critical: ${bodyJSON["critical"]}
+Recovered: ${bodyJSON["todayRecovered"]}
+
+Total
+Deaths: ${bodyJSON["deaths"]}
+Active: ${bodyJSON["active"]}
+Cases: ${bodyJSON["cases"]}
+Tests: ${bodyJSON["tests"]}
+Recovered: ${bodyJSON["recovered"]}
+`;
+            bot.sendPhoto(chatId, bodyJSON["countryInfo"]["flag"], {caption: covidData});
+            bot.sendMessage(chatId, "Done!", commands);
         } else {
             console.log("error");
         }
     })
 }
+
+
+// Crypto API
+async function getCryptoData(chatId) {
+    let data = await CoinGeckoClient.ping();
+    let data2 = await CoinGeckoClient.coins.all();
+    for(var i = 0; i < 10; i++){
+        let data4 = await CoinGeckoClient.coins.fetch(data2["data"][i]["id"], {});
+
+let cryptoData = `
+${data2["data"][i]["market_data"]["market_cap_rank"]}. ${data2["data"][i]["name"]} - ${data2["data"][i]["symbol"]}
+
+Price: \$${data2["data"][i]["market_data"]["current_price"]["usd"]}
+High (24hr): \$${data2["data"][i]["market_data"]["high_24h"]["usd"]}
+Low (24hr): \$${data2["data"][i]["market_data"]["low_24h"]["usd"]}
+Change (24hr): ${data2["data"][i]["market_data"]["price_change_percentage_24h"]}%
+Total Supply: \$${data2["data"][i]["market_data"]["total_supply"]}
+Circulating Supply: \$${data2["data"][i]["market_data"]["circulating_supply"]}
+
+Description:
+${data4["data"]["description"]["en"].substring(0,830)}
+`;
+        await bot.sendPhoto(chatId, data2["data"][i]["image"]["large"], {caption: cryptoData});
+    }
+    bot.sendMessage(chatId, "Done!", commands);
+    //console.log(data);
+}
+
+
+// Covid API
+async function covidAPI(chatId)  {
+    await request('https://corona.lmao.ninja/v2/countries/Ethiopia', function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            let bodyJSON = JSON.parse(body);
+            let covidData = "COVID CASES \n\n" + bodyJSON["country"] + "\n\nToday: " + bodyJSON["todayCases"] + "\nTotal: " + bodyJSON["cases"];
+covidData = `
+${bodyJSON["country"]}
+
+Today
+Deaths: ${bodyJSON["todayDeaths"]}
+Cases: ${bodyJSON["todayCases"]}
+Critical: ${bodyJSON["critical"]}
+Recovered: ${bodyJSON["todayRecovered"]}
+
+Total
+Deaths: ${bodyJSON["deaths"]}
+Active: ${bodyJSON["active"]}
+Cases: ${bodyJSON["cases"]}
+Tests: ${bodyJSON["tests"]}
+Recovered: ${bodyJSON["recovered"]}
+`;
+            bot.sendPhoto(chatId, bodyJSON["countryInfo"]["flag"], {caption: covidData});
+            bot.sendMessage(chatId, "Done!", commands);
+        } else {
+            console.log("error");
+        }
+    })
+}
+
+
 // Convo
 bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
@@ -37,8 +130,21 @@ bot.on("message", async (msg) => {
     // Hello
     let greetings = "Hello 👋 \n" +  msg.chat.first_name.toString() + ".";
     if(msgReceived.includes("hi") || msgReceived.includes("hello") || msgReceived.includes("hey")){
-        bot.sendMessage(chatId, greetings);
+        bot.sendMessage(chatId, greetings, commands);
     }
+
+    // Good Morning
+    let goodMorning = "Good Morning! 🔆";
+    if(msgReceived.includes("good morning") || msgReceived.includes("goodmorning") || msgReceived.includes("morning") || msgReceived.includes("gm")){
+        bot.sendPhoto(chatId, "assets/picture/morning3.jpg", {caption: goodMorning});
+    }
+
+    // Good Night
+    let goodNight = "Good night 🌙";
+    if(msgReceived.includes("good night") || msgReceived.includes("goodnight") || msgReceived.includes("night") || msgReceived.includes("gn")){
+        bot.sendPhoto(chatId, "assets/picture/moon3.jpg", {caption: goodNight});
+    }
+
     // Bye
     let bye = "Good Bye, Have a nice time!";
     if(msgReceived.includes("bye")){
@@ -61,29 +167,19 @@ bot.on("message", async (msg) => {
         //let songLink = msgReceived.substring(8, msgReceived.length);
         bot.sendMessage(chatId, "Sending Song...");
         await bot.sendAudio(chatId, "assets/music/bornSinner.mp3");
-        bot.sendMessage(chatId, "Done!");
+        bot.sendMessage(chatId, "Done!", commands);
     }
 
-    //
+    // Covid Stats
     if(msgReceived.includes("covid")){
-        //let songLink = msgReceived.substring(8, msgReceived.length);
-        bot.sendMessage(chatId, "Sending Song...");
-        //await covidAPI();
-        await request.get('https://corona.lmao.ninja/v2/countries/Ethiopia', function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log(body);
-                let cases = body["cases"].toString();
-                console.log(cases);
+        bot.sendMessage(chatId, "Sending Covid Stats...");
+        await covidAPI(chatId);
+    }
 
-                covidData = body;
-            } else {
-                console.log("error");
-            }
-        })
-        bot.sendMessage(chatId, covidData);
-        
-        
-        bot.sendMessage(chatId, "Done!");
+    // Crypto Prices
+    if(msgReceived.includes("crypto")){
+        bot.sendMessage(chatId, "Getting Crypto Prices...");
+        await getCryptoData(chatId);
     }
     
 });
